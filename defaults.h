ade6590655f7e761ac4b40a541f303ab7723be7d
TomAgacci <dementedmax@gmail.com>
2026-07-07 15:39:43 -0700

    Create defaults.h

---
+++ defaults.h
@@ -0,0 +1,11 @@
+#ifndef defaults_h
+#define defaults_h
+
+#define DEFAULT_X_MAX_TRAVEL 200.0
+#define DEFAULT_Y_MAX_TRAVEL 200.0
+#define DEFAULT_Z_MAX_TRAVEL 200.0
+
+#define DEFAULT_HOMING_DIR_MASK 0
+#define DEFAULT_HOMING_FEEDRATE 500.0
+
+#endif
