e07e8d194cf403c4d9dc97ce05dc330aee31b227
TomAgacci <dementedmax@gmail.com>
2026-07-07 16:05:45 -0700

    Create heater.h

---
+++ nano-selector-grbl/heater.h
@@ -0,0 +1,3 @@
+// Optional: if you want heater logic separated.
+// Currently, heater functions are in system.c.
+// You can leave this file empty or move heater_loop(), thermistor_to_celsius() here.
